1c407b3281d8d91ebd15d98ce1158662c70d882e
Juliana8b <167132725+Juliana8b@users.noreply.github.com>
2024-05-01 15:19:40 -0500

    JS improved

--- src/app.js
+++ src/app.js
@@ -1,28 +1,16 @@
 function showTemperature(response) {
   let temperatureElement = document.querySelector(".current-temperature-value");
-  let temperature = Math.round(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("h1.current-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = `${temperature}`;
   let conditionElement = document.querySelector("#condition");
-  let condition = response.data.condition.description;
-  conditionElement.innerHTML = `${condition}`;
   let humidityElement = document.querySelector("#currentHumidity");
-  let humidity = response.data.temperature.humidity;
-  humidityElement.innerHTML = `${humidity}%`;
-  let windElement = document.querySelector(".current-wind");
-  let wind = response.data.wind.speed;
-  windElement.innerHTML = `${wind}`;
-}
+  let windElement = document.querySelector("#current-wind");
 
-function changeCity(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  let cityName = document.querySelector("h1.current-city");
-  cityName.innerHTML = cityInput.value;
-  let apiKey = "a0db461tcf90de7688b6aab936fao1ed";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}&unit=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 let now = new Date();
@@ -38,15 +26,26 @@ function dateTime() {
   ];
 
   let day = days[now.getDay()];
-
   let hour = now.getHours();
   let minute = now.getMinutes().toString().padStart(2, "0");
-
-  let curTime = `${day} ${hour}:${minute}`;
   let currentDetails = document.querySelector("#day-time");
-  currentDetails.innerHTML = curTime;
+  currentDetails.innerHTML = `${day} ${hour}:${minute}`;
 }
-dateTime();
 
-let selectCity = document.querySelector("#search-form");
-selectCity.addEventListener("submit", changeCity);
+function citySearch(city) {
+  let apiKey = "a0db461tcf90de7688b6aab936fao1ed";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function changeCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  citySearch(cityInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", changeCity);
+
+citySearch("Toronto");
+dateTime();
