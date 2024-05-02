2710938fe4854f7a149c0c0450b2c8287d2f664f
Juliana8b <167132725+Juliana8b@users.noreply.github.com>
2024-05-02 18:17:38 -0500

    updated js, so forecast starting from next day

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ function getForecast(city) {
 function displayForecast(response) {
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day, index) {
+  response.data.daily.slice(1).forEach(function (day, index) {
     if (index < 5) {
       forecastHtml =
         forecastHtml +
